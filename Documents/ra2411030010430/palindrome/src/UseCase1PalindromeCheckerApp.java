60e458823e834aa83522d7a6ef0ec8511dc522d0
PRATYAKSH344 <dharunmurugan1@gmail.com>
2026-03-11 09:34:02 +0530

    welcome msg

--- Documents/ra2411030010430/palindrome/src/UseCase1PalindromeCheckerApp.java
+++ Documents/ra2411030010430/palindrome/src/UseCase1PalindromeCheckerApp.java
@@ -1,1 +1,5 @@
-
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welocme to palindrome checker manager system\nVersion:1.8\nsystem initalized succesfully");
+    }
+}
